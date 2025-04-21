689292e00bf171c9e193d825e6c10eb0665952be
germouai <germangabriel.mendezciancaglini@alumnos.uai.edu.ar>
2025-04-21 19:36:51 -0300

    Add project files.
    Add .gitattributes and .gitignore.
    
    Initial commit

---
+++ TPI-ProjectPresenter/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TPI_ProjectPresenter.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
