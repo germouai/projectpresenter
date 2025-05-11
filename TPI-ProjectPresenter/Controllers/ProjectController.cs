0b498bda1df2dcf7637bf4628c531773b7f57ea6
GermoUAI <germangabriel.mendezciancaglini@alumnos.uai.edu.ar>
2025-05-11 19:35:53 -0300

    Lista de proyectosa como pagina principal. Cambio de controller para pagina principal.

--- TPI-ProjectPresenter/Controllers/ProjectController.cs
+++ TPI-ProjectPresenter/Controllers/ProjectController.cs
@@ -23,8 +23,13 @@ namespace TPI_ProjectPresenter.Controllers
 
             var projectest = new ProjectEntity();
 
-            projectest.Header = new ProjectEntity.ProjectHeader() { ProjectDescription = "Nos this is the one to make extensive, at least for the testing. You know, maybe it fucks up responsiveness or sth. You can never know until it's fucked up.",
-                                                                    ProjectImgRef = "ideas.png", ProjectName = "Testing the project title", ProjectTooltip = "This should be a considerably long text but not so much, it's small and in italicz."  };
+            projectest.Header = new ProjectEntity.ProjectHeader() 
+            { 
+                ProjectDescription = "Nos this is the one to make extensive, at least for the testing. You know, maybe it fucks up responsiveness or sth. You can never know until it's fucked up.",
+                ProjectImgRef = "ideas.png", 
+                ProjectName = "Testing the project title", 
+                ProjectTooltip = "This should be a considerably long text but not so much, it's small and in italicz."  
+            };
 
             var tabtest = new ProjectTab();
             tabtest.TabName = "Basic Info";
@@ -33,6 +38,21 @@ namespace TPI_ProjectPresenter.Controllers
             projectest.AddTab(tabtest);
 
             return View(projectest);
+        }
+
+        public IActionResult ProjectList()
+        {
+			var projectest = new ProjectEntity();
+
+			projectest.Header = new ProjectEntity.ProjectHeader()
+			{
+				ProjectDescription = "Nos this is the one to make extensive, at least for the testing. You know, maybe it fucks up responsiveness or sth. You can never know until it's fucked up.",
+				ProjectImgRef = "ideas.png",
+				ProjectName = "Testing the project title",
+				ProjectTooltip = "This should be a considerably long text but not so much, it's small and in italicz."
+			};
+
+			return View(projectest);
         }
 	}
 }
--- TPI-ProjectPresenter/Program.cs
+++ TPI-ProjectPresenter/Program.cs
@@ -22,6 +22,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Project}/{action=ProjectList}/{id?}");
 
 app.Run();
